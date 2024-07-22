46198595c0caf99d02bfe08957533f62f6dd5696
Tunahan Karyağdı <thankaryagdi@gmail.com>
2024-07-22 20:41:16 +0300

    add: fibonacci

---
+++ FirstProject/app/src/main/java/com/tunahankaryagdi/firstproject/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.tunahankaryagdi.firstproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var firstValue = 1
+        var secondValue = 1
+
+        while(firstValue < 100){
+            println(firstValue)
+            val temp = firstValue
+            firstValue = secondValue
+            secondValue += temp
+        }
+
+    }
+}
