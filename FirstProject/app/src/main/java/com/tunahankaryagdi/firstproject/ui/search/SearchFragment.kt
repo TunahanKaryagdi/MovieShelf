0f1f7963540268db0406e01701364fae72933704
Tunahan Karyağdı <thankaryagdi@gmail.com>
2024-08-06 16:11:13 +0300

    add: search fragment, search view model

---
+++ FirstProject/app/src/main/java/com/tunahankaryagdi/firstproject/ui/search/SearchFragment.kt
@@ -0,0 +1,10 @@
+package com.tunahankaryagdi.firstproject.ui.search
+
+import androidx.fragment.app.Fragment
+import com.tunahankaryagdi.firstproject.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SearchFragment : Fragment(R.layout.fragment_search) {
+
+}
---
+++ FirstProject/app/src/main/java/com/tunahankaryagdi/firstproject/ui/search/adapter/SearchMovieListAdapter.kt
@@ -0,0 +1,42 @@
+package com.tunahankaryagdi.firstproject.ui.search.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.tunahankaryagdi.firstproject.databinding.ItemSearchListBinding
+import com.tunahankaryagdi.firstproject.domain.model.Movie
+
+class SearchMovieListAdapter(
+    val onClickMovie: (Int) -> Unit
+) :
+    PagingDataAdapter<Movie, SearchMovieListAdapter.SearchMovieListViewHolder>(MovieDiffCallback()) {
+
+    class SearchMovieListViewHolder(internal val binding: ItemSearchListBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchMovieListViewHolder {
+        val binding =
+            ItemSearchListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return SearchMovieListViewHolder(binding)
+    }
+
+
+    override fun onBindViewHolder(holder: SearchMovieListViewHolder, position: Int) {
+        val movie = getItem(position) ?: return
+
+    }
+
+
+    class MovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+}
